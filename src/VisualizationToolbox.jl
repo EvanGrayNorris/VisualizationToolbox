228681e00a1b19589665eb76f2b72b48dec27e65
Evan Norris <evangnorris@gmail.com>
2022-04-19 10:22:06 -0400

    test, updating

--- src/VisualizationToolbox.jl
+++ src/VisualizationToolbox.jl
@@ -38,7 +38,7 @@ module VisualizationToolbox
         """
             plot_fibrils(;fibril_dataset)
 
-        return a plot of fibrils defined by the input dataframe
+        return a plot of fibrils defined by the input dataframe, update
         """
         function plot_fibrils(;fibril_dataset)
             return "blank"
